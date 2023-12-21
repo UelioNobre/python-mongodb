2a6f603ebfd551c9ab7f48e3a6b41c4498039eac
uelionobre <uelionobre@hotmail.com>
2023-12-21 19:41:53 -0300

    Exibe alguns resultados usando o perados $and

---
+++ app2/src/exercicio-7.py
@@ -0,0 +1,33 @@
+from rich import print
+from pymongo import MongoClient
+
+
+# Retorne o nome da acomodação (name), o resumo da
+# acomodação (summary), seu preço (price) e todos as
+# notas de reviews (review_score) dos superhosts
+# (host.host_is_superhost) com número de reviews
+# (number_of_reviews) superior a 500
+with MongoClient() as client:
+    db = client.project
+
+    query = {
+        "$and": [
+            {"host.host_is_superhost": True},
+            {"number_of_reviews": {"$gte": 500}},
+        ]
+    }
+    query = {
+        "$and": [
+            {"host.host_is_superhost": True},
+            {"number_of_reviews": {"$gte": 500}},
+        ]
+    }
+    projection = {
+        "name": True,
+        "summary": True,
+        "price": True,
+        "number_of_reviews": True,
+    }
+
+    for document in db.listing.find(query, projection):
+        print(document)
