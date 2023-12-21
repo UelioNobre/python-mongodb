6ecfc546e24c1251421a109c8fbc3d23e1f46968
uelionobre <uelionobre@hotmail.com>
2023-12-21 18:46:33 -0300

    Exibe nome, numero de reviews e notas dos 500 primeiros baseados na query

---
+++ app2/src/exercicio-5.py
@@ -0,0 +1,31 @@
+from rich import print
+from pymongo import MongoClient
+
+
+# Retorne o nome, número de reviews (number_of_reviews) e as
+# notas dos TODOS os reviews dos 500 primeiros valores que
+# tiverem número de reviews maior que 300 ou nota dos reviews
+# (review_scores.review_scores_rating) maior ou igual a 95
+# ordenado por maior quantidade de reviews, maior nota de
+# reviews e ordem alfabética
+with MongoClient() as client:
+    db = client.project
+
+    query = {
+        "number_of_reviews": {"$gt": 300},
+        "review_scores.review_scores_rating": {"$gte": 95},
+    }
+
+    projection = {
+        "name": True,
+        "number_of_reviews": True,
+        "review_scores.review_scores_rating": True,
+    }
+
+    sort = [
+        ("number_of_reviews", -1),
+        ("review_scores.review_scores_rating", 1),
+    ]
+
+    for document in db.listing.find(query, projection, sort=sort).limit(500):
+        print(document)
