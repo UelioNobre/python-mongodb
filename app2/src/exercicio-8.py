3334b9ba219fa236e5c400613fc3b8eb9d52e14a
uelionobre <uelionobre@hotmail.com>
2023-12-22 12:18:22 -0300

    Exibe documentos ordenados pelo maior número de reviews, menor preço e em ordem alfabética

--- app2/src/exercicio-8.py
+++ app2/src/exercicio-8.py
@@ -1,16 +1,30 @@
 from rich import print
 from pymongo import MongoClient
 
+# Retorne o nome (name), a descrição (description), o número de reviews
+# (number_of_reviews), as facilidades (amenities) e o preço (price) das
+# acomodações que aceitam pets (amenities deve conter “Pets allowed”)
+# ordenado pelo maior número de reviews, menor preço e em ordem alfabética
 with MongoClient() as client:
     db = client.project
 
-    # db.listing.find({ amenities: { $in: ["Pets allowed"] } }, { name: 1, number_of_reviews: 1, price: 1, amenities: 1, _id: 0 }).sort({ number_of_reviews: -1, price: 1, name: 1 })
+    query = {
+        "amenities": {
+            "$in": ["Pets allowed"],
+        }
+    }
+    projection = {
+        "name": 1,
+        "description": 1,
+        "number_of_reviews": 1,
+        "price": 1,
+        "amenities": 1,
+    }
+    sort = [
+        ("number_of_reviews", -1),
+        ("price", 1),
+        ("name", 1),
+    ]
 
-    query = {"amenities": {"$in": ["Pets allowed"]}}
-    projection = {}
-    sort = []
-
-    for document in db.listing.find(query, projection).limit(1):
+    for document in db.listing.find(query, projection, sort=sort):
         print(document)
-
-print("Fim")
